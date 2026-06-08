06f142c21b79fe77e804e2501936675d3123fbc2
lukas-sgx <lukas.soigneux@epitech.eu>
2026-06-09 00:08:02 +0200

    style: set indent on 4 spaces

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const port = 58925;
 
+app.use(express.json());
+
 app.post('/', (req, res) => {
-  console.log(req.body)
-  res.send("Receive request");
+    console.log(req.body)
+    res.send("Receive request");
 });
 
-app.listen(port, () => {
-  console.log(`Dispatch app listening on port ${port}`);
+app.listen(port, async () => {
+    console.log(`Dispatch app listening on port ${port}`);
 });
